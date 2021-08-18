1a0699239bf382a793a63a27d54750c1eab10098
Tommy Zarick <zarick.6@osu.edu>
2021-08-18 01:34:57 -0400

    test case setup

--- src/evenUp.test.ts
+++ src/evenUp.test.ts
@@ -1,6 +1,8 @@
 describe('evenUp', () => {
   describe('calculateDebts()', () => {
     it('should work properly', () => {
+      const inputPayments = [60, 75, 20, 0];
+      const expectedDebts = [-21.25, -36.25, 18.75, 38.75];
       expect('ahoy').toEqual('ahoy');
     });
   });
