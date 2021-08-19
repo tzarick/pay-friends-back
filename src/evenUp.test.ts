e92bf59950b7d4f7e2ce031f26f84335e65a7a3d
Tommy Zarick <zarick.6@osu.edu>
2021-08-19 00:43:09 -0400

    calculate debts and build transaction map - working

--- src/evenUp.test.ts
+++ src/evenUp.test.ts
@@ -1,9 +1,43 @@
+import { buildTransactionMap, calculateDebts } from './evenUp';
+
 describe('evenUp', () => {
   describe('calculateDebts()', () => {
-    it('should work properly', () => {
+    test('case 1', () => {
       const inputPayments = [60, 75, 20, 0];
       const expectedDebts = [-21.25, -36.25, 18.75, 38.75];
-      expect('ahoy').toEqual('ahoy');
+
+      expect(calculateDebts(inputPayments)).toEqual(expectedDebts);
+    });
+    test('case 2', () => {
+      const inputPayments = [100, 72.5, 20, 7, 0];
+      const expectedDebts = [-60.1, -32.6, 19.9, 32.9, 39.9];
+
+      expect(calculateDebts(inputPayments)).toEqual(expectedDebts);
+    });
+  });
+
+  describe('buildTransactionMap()', () => {
+    test('case 1', () => {
+      const inputDebts = [-21.25, -36.25, 18.75, 38.75];
+      const friends = ['mitch', 'johnny', 'beth', 'randy'];
+
+      const expectedTransactionMap = [
+        'randy pays johnny $36.25',
+        'randy pays mitch $2.50',
+        'beth pays mitch $18.75',
+      ];
+
+      expect(buildTransactionMap(inputDebts, friends)).toEqual(
+        expectedTransactionMap
+      );
+    });
+    it('should not allow uneven debts', () => {
+      const inputDebts = [32, 12, -1];
+      const friends = ['bailey', 'winston', 'jess'];
+
+      expect(() => {
+        buildTransactionMap(inputDebts, friends);
+      }).toThrow('The sum of all debts must be zero');
     });
   });
 });
--- src/evenUp.ts
+++ src/evenUp.ts
@@ -1,5 +1,7 @@
 import prompt from 'prompt';
 import colors from 'colors/safe';
+import assert from 'assert';
+import { Color } from 'colors';
 
 const MESSAGE_COLOR = colors.cyan;
 
@@ -59,21 +61,111 @@ const getFriendNamesAndPayments = async (): Promise<{
   return inputObj;
 };
 
-// TODO
-const calculateDebts = (payments: number[]) => {};
+export const calculateDebts = (payments: number[]) => {
+  const total = payments.reduce((a, b) => a + b, 0);
+  const evenAmount = total / payments.length;
+
+  const debts = payments.map((item) => evenAmount - item);
+
+  return debts;
+};
+
+// sort debts from smallest to largest (neg to pos == [is owed] to [owes])
+const sortFriendsByDebt = (friends: string[], debts: number[]) => {
+  let friendDebts: { name: string; debt: number }[] = [];
+  friends.forEach((item, i) => {
+    friendDebts.push({
+      name: item,
+      debt: debts[i],
+    });
+  });
+
+  friendDebts = friendDebts.sort((a, b) => a.debt - b.debt); // asc
+  const friendsSorted = friendDebts.map((item) => item.name);
+  const debtsSorted = friendDebts.map((item) => item.debt);
+
+  return {
+    friendsSorted,
+    debtsSorted,
+  };
+};
+
+/**
+ * Build a transaction map that evens out the input debts
+ * @param debts array of debts
+ * @returns transaction map - a list of transactions between friends to even up
+ */
+export const buildTransactionMap = (
+  debts: number[],
+  friends: string[]
+): string[] => {
+  assert(
+    debts.reduce((a, b) => a + b, 0) === 0,
+    'The sum of all debts must be zero'
+  );
+
+  const { friendsSorted, debtsSorted } = sortFriendsByDebt(friends, debts);
+
+  let transactions: string[] = [];
+
+  let i = 0;
+  let j = debts.length - 1;
+  while (i < j) {
+    let largerAbsValIdx = -1;
+    let smallerAbsValIdx = -1;
+
+    if (Math.abs(debtsSorted[i]) <= Math.abs(debtsSorted[j])) {
+      smallerAbsValIdx = i;
+      largerAbsValIdx = j;
+    } else {
+      smallerAbsValIdx = j;
+      largerAbsValIdx = i;
+    }
+
+    const txAmount = Math.abs(debtsSorted[smallerAbsValIdx]);
+
+    // adjust values based on transaction amount. Make sure our signs are right by checking whether or not the left side is positive or negative (aka i or j -> implicit: i should always be neg [is owed], j should always be pos [owes])
+    debtsSorted[largerAbsValIdx] -=
+      largerAbsValIdx === i ? -txAmount : txAmount;
+    debtsSorted[smallerAbsValIdx] = 0;
+
+    const txAmountDisplay = txAmount % 1 === 0 ? txAmount : txAmount.toFixed(2); // int vs float
+    transactions.push(
+      `${friendsSorted[j]} pays ${friendsSorted[i]} $${txAmountDisplay}`
+    );
+
+    if (debtsSorted[i] === 0) i++;
+    if (debtsSorted[j] === 0) j--;
+  }
+
+  return transactions;
+};
+
+const displayTransactions = (
+  transactions: string[],
+  color?: (x: string) => string
+) => {
+  console.log('\n');
+
+  transactions.forEach((tx) => {
+    const line = color ? color(tx) : tx;
+    console.log('>>', line);
+  });
+};
 
 export const evenUp = async () => {
-  try {
-    const namesAndPayments = await getFriendNamesAndPayments();
-    console.log(namesAndPayments);
-    const friends = Object.keys(namesAndPayments);
-    const payments = Object.values(namesAndPayments);
+  const namesAndPayments = await getFriendNamesAndPayments();
 
-    console.log(`Friends: ${friends}`);
-    console.log(`Payments: ${payments}`);
+  const friends = Object.keys(namesAndPayments);
+  const payments = Object.values(namesAndPayments);
 
-    const debts = calculateDebts(payments); // TODO
-  } catch (_) {
-    console.log('Something went wrong while evening up :( Please try again!');
+  if (friends.length <= 1) {
+    console.log('Need more friends, sorry.');
+    return;
   }
+
+  const debts = calculateDebts(payments);
+
+  const transactions = buildTransactionMap(debts, friends);
+  displayTransactions(transactions, colors.bold);
 };
